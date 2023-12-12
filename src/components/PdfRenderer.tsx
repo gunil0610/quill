466da65db881a292a7f3909549341f861e031b63
gunil0610 <gunil.na1@gmail.com>
2023-12-13 02:26:46 +0900

    feat: dashboard file detail page start

---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      pdfrenderer
+    </div>
+  );
+};
+
+export default PdfRenderer;
